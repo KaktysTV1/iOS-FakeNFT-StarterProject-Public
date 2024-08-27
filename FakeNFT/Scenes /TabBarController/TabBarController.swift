c52dc9d07683f2cb4bb87f2b73f0f03d76d2c032
KaktysTV1 <wotmaster187@iclod.com>
2024-08-27 13:17:19 +0300

    Исправлено критическое замечание

--- FakeNFT/Scenes /TabBarController/TabBarController.swift
+++ FakeNFT/Scenes /TabBarController/TabBarController.swift
@@ -5,7 +5,7 @@ final class TabBarController: UITabBarController {
     var servicesAssembly: ServicesAssembly!
 
     private let catalogTabBarItem = UITabBarItem(
-        title: "Каталог",
+        title: NSLocalizedString("Tab.catalog", comment: ""),
         image: UIImage(systemName: "square.stack.3d.up.fill"),
         tag: 0
     )
--- FakeNFT/Services/Requests/RequestConstants.swift
+++ FakeNFT/Services/Requests/RequestConstants.swift
@@ -1,5 +1,5 @@
 enum RequestConstants {
     static let baseURL = "https://d5dn3j2ouj72b0ejucbl.apigw.yandexcloud.net"
     #warning("Instert your token here")
-    static let token = ""
+    static let token = "f2d94f2b-83ca-42c1-ab75-f7f4571e0e06"
 }
